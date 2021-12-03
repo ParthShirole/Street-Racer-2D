177dfe69ad87b21a66658dd87131fa28593d5938
KunalA18 <kunalagarwal1072002@gmail.com>
2021-12-04 02:24:46 +0530

    added enemy car and collison with it

--- my_game/game.js
+++ my_game/game.js
@@ -1,14 +1,3 @@
-// var config = {
-//     type: Phaser.AUTO,
-//     width: 1920,
-//     height: 900,
-//     scene: {
-//         preload: preload,
-//         create: create,
-//         update: update
-//     }
-// };
-
 window.onload = function() {
     let gameConfig = {
         type: Phaser.AUTO,
@@ -36,33 +25,54 @@ class playGame extends Phaser.Scene{
     preload(){
     this.load.image('road', 'assets/road.png');
     this.load.image('player', 'assets/player.png');
+    this.load.image('enemy', 'assets/enemycar.png');
     }
 
     create(){
-    this.add.image(this.game.scale.width/2, this.game.scale.height/2, 'road');
-    this.add.image(this.game.scale.width/2, this.game.scale.height/2, 'player');
+    this.road=this.add.tileSprite(this.game.scale.width/2, this.game.scale.height/2,0,0, 'road');
+    // this.add.image(this.game.scale.width/2, 9*this.game.scale.height/10, 'player');
+    this.player = this.physics.add.sprite(this.game.scale.width/2, 9*this.game.scale.height/10, 'player');
+    // this.physics.world.setBounds(0, 0, this.game.scale.width, this.game.scale.height);
+    this.enemy=this.physics.add.sprite(800,300,'enemy')
+    // this.enemy=this.add.tileSprite(800,300,0,0,'enemy')
+    this.player.setCollideWorldBounds(true);
+    this.enemy.setCollideWorldBounds(true);
+    
     
     this.cursors = this.input.keyboard.createCursorKeys();
-    this.speed = 5;
-    // this.scale.x = 0.7;
-    // this.scale.y = 0.7;
+    this.physics.add.collider(this.player, this.road);
+    this.physics.add.collider(this.enemy, this.road);
+    this.physics.add.collider(this.player, this.enemy);
+    
+    // this.speed = 5;
+    
     }
 
     update(){
     if (this.cursors.left.isDown) {
-        this.x -= this.speed;
+        this.player.setVelocityX(-500);
         }
 
     else if (this.cursors.right.isDown) {
-        this.x += this.speed;
+        this.player.setVelocityX(500);
         }
 
+    else{
+        this.player.setVelocityX(0);
+    }
+
     if (this.cursors.up.isDown) {
-        this.y -= this.speed;
+        this.player.setVelocityY(-500);
         }
     else if (this.cursors.down.isDown) {
-        this.y += this.speed;
+        this.player.setVelocityY(500);
         }
+    
+    else{
+        this.player.setVelocityY(0);
+    }
+    // this.enemy.tilePositionY -= 5;
+    this.road.tilePositionY -= 10;
 
     }
 }
