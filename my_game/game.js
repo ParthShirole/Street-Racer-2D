336d5468c93778b6605e88a1da965dd8a1d9a93b
Parth Shirole <parthshirole06@gmail.com>
2022-01-07 20:06:45 +0530

    Implemented WASD for controls

--- my_game/game.js
+++ my_game/game.js
@@ -129,7 +129,11 @@ class playGame extends Phaser.Scene{
 
     createCursor(){
         // Initializing the cursor keys
-        this.cursors = this.input.keyboard.createCursorKeys();
+        this.cursors = this.input.keyboard.addKeys(
+            {up:Phaser.Input.Keyboard.KeyCodes.W,
+            down:Phaser.Input.Keyboard.KeyCodes.S,
+            left:Phaser.Input.Keyboard.KeyCodes.A,
+            right:Phaser.Input.Keyboard.KeyCodes.D});
 
 
         // If left arrow key is pressed
